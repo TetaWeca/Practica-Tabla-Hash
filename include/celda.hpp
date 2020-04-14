8c348b44f4809b4928338b4c0dd35f8fece0c821
TetaWeca <alu0101206646@ull.edu.es>
2020-04-14 10:59:39 +0100

    First Commit

---
+++ include/celda.hpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+class cell 
+{
+protected:
+
+    int cellsize;
+
+public:
+
+    bool search(int x);
+
+    void insert(int x);
+
+    bool isFull();
+};
---
+++ include/tabla.hpp
@@ -0,0 +1,21 @@
+
+#include "celda.hpp"
+#include <iostream>
+
+class HashMap 
+{
+    protected:
+        
+        int vcelda;
+        int nceldas;
+        int fdispersion;
+        int fExploracion;
+    
+    public:
+
+        bool search(int x);
+
+        bool insert(int x);
+
+
+    };
